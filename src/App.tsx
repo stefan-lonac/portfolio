868e61482720bc53674a48a3e6780af9d93e70de
stefan-lonac <loncaric.stefan@gmail.com>
2025-03-05 13:14:33 +0100

    [Added] About me and Information section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import AboutMe from './components/about-me/AboutMe'
 import Header from './components/header/Header'
+import Information from './components/information/Information'
 
 function App() {
   return (
     <>
       <Header />
+      <main className="mx-[auto] px-6 w-[100%] md:max-w-screen-md lg:max-w-screen-xl">
+        <Information />
+        <AboutMe />
+      </main>
     </>
   )
 }
